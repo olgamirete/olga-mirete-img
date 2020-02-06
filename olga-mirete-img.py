468b99c5b8dc6d99da61ac42d11c2b3f43f70cfd
olgamirete <olgamirete@gmail.com>
2020-02-06 11:13:41 -0300

    removed unnecesary line

--- olga-mirete-img.py
+++ olga-mirete-img.py
@@ -21,7 +21,6 @@ for row in range(height):
     for col in range(width):
         draw.point((row, col), rgb2hex(row*3*100/scale, col*3*100/scale, (row+col)*100/scale))
 
-text_to_draw = 'This is a test.'
 text_to_draw = 'This is a longer test that will help see how this is going to be handled.'
 draw.text((10, 10), text_to_draw, rgb2hex(255, 255, 255), font)
 
